0ba29cf79e4059850ab11aecab001e71ac0a5475
Juliakp1 <juliakp710@gmail.com>
2023-05-09 09:49:40 -0300

    attempting back+front

--- journey_front/src/components/Form.jsx
+++ journey_front/src/components/Form.jsx
@@ -11,16 +11,42 @@ import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
 
 const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#77dd77',
-      },
-      secondary: {
-        main: '#fff',
-      },
+  palette: {
+    primary: {
+      main: '#77dd77',
     },
-  });
+    secondary: {
+      main: '#fff',
+    },
+  },
+});
 
+const handleClick = searchViag => {
+
+  const data = {
+      'origem': origem,
+      'destino': destino,
+      'idMotorista': 123,
+      'dataStart': 1,
+  }
+
+  fetch('http://localhost:8080/viagem', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+  }).then(response => {
+      if (response.status === 200) {
+          setMensagem('Viagens Encontradas: ')
+          setOpen(true)
+      }
+  }).catch(ex => {
+      setMensagem('Erro ao achar viagens')
+      setOpen(true)
+  })
+
+}
 
 function Form() {
 
@@ -42,12 +68,14 @@ function Form() {
         <Box sx={{display:'flex',flexDirection:'column',gap:2,padding:10,width:700}} className='form' component={Paper}>
             {/* <Map></Map> */}
             <ResponsiveDateTimePickers></ResponsiveDateTimePickers>
-            <input type="text" placeholder="Origem" className='origem'/>
-            <input type="text" placeholder="Destino" className='destino'/>
-            <Button type="submit" theme={theme} variant='contained' className='buscando' onClick={() => {setCarregando(true); change()}}>
-      <AddIcon></AddIcon>
-        Buscar Viagem
-      </Button>
+            <input type="text" variant="standard" placeholder="Origem" className='origem' label='origem' onChange={searchViag => setOrigem(searchViag.target.value)}/>
+            <input type="text" variant="standard" placeholder="Destino" className='destino' label='destino' onChange={searchViag => setDestino(searchViag.target.value)}/>
+
+            {/* onClick={() => {setCarregando(true); change()}} */}
+            <Button type="submit" theme={theme} variant='contained' className='buscando' onClick={handleClick} >
+            <AddIcon></AddIcon>
+              Buscar Viagem
+            </Button>
         </Box>
         </div>
 
